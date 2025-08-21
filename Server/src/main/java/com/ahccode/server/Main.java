0bc9b6222b9bea4afb442fc78507b6031db27d72
Ali-Haris-Chishti <harischishti28@outlook.com>
2025-08-21 15:58:41 +0500

    some minor fixes

--- Server/src/main/java/com/ahccode/server/Main.java
+++ Server/src/main/java/com/ahccode/server/Main.java
@@ -1,9 +1,24 @@
 package com.ahccode.server;
 
 import com.ahccode.server.ui.GameServerManagerCLI;
+import com.ahccode.server.ui.GameServerManagerUI;
 
 public class Main {
     public static void main(String[] args) {
-        GameServerManagerCLI.main(args);
+        switch (parseMode(args)) {
+            case "gui" -> GameServerManagerUI.main(args);
+            case "cli" -> GameServerManagerCLI.main(args);
+            case null, default -> throw new IllegalArgumentException("Unknown arguments");
+        }
     }
+
+    public static String parseMode(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals("-m") || args[i].equals("--mode")) {
+                return args[++i];
+            }
+        }
+        return null;
+    }
+
 }
--- Server/src/main/java/module-info.java
+++ Server/src/main/java/module-info.java
@@ -3,4 +3,6 @@ module com.ahccode.server {
     requires org.slf4j;
     requires javafx.controls;
     requires com.ahccode.common;
+
+    exports com.ahccode.server.ui to javafx.graphics;
 }
